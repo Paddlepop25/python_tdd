6de51081945d0685e31548f55af57ad4c9aa68f7
Ubuntu <ubuntu@ip-172-31-85-204.ec2.internal>
2019-10-23 07:47:12 +0000

    completed evens test

--- evens.py
+++ evens.py
@@ -1,6 +1,24 @@
 def even_number_of_evens(numbers):
-    return False
+    if len(numbers) < 2:
+        return False
+    elif len(numbers) >= 2:  
+        count = 0
+        for i in numbers:
+            if i % 2 == 0:
+                count += 1
+        if count < 1:
+            return False
+        elif count % 2 == 0:
+            return True
+        else:        
+            return False
 
 assert even_number_of_evens([]) == False, "No numbers"
+assert even_number_of_evens([2]) == False, "One even number"
+assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four are even"
+assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
 print("All tests passed!")
