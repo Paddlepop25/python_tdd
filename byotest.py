9ef1f1758e011416b294b442ff8385213cfde59c
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-10-24 07:15:57 +0000

    added tests tovending_machine program

--- byotest.py
+++ byotest.py
@@ -13,9 +13,8 @@ def test_not_in(item, collection):
 def test_between(lower_limit, upper_limit, num):
     assert(lower_limit <= num <= upper_limit), "{0} is not between {1} and {2}".format(num, lower_limit, upper_limit)
 
-test_are_equal(1, 2)
-test_not_equal("1", 1)
-test_is_in([1, 3, 4], 2)
-test_not_in(2, [1,2,3,4])
-test_between(1, 5, 6)
-print("Passed all tests")
+# test_are_equal(1, 2)
+# test_not_equal("1", 1)
+# test_is_in([1, 3, 4], 2)
+# test_not_in(2, [1,2,3,4])
+# test_between(1, 5, 6)
